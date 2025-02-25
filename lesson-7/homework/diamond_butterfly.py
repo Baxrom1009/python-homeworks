32138d627e3c33877bcc9b1eebe5db174097e706
Baxrom1009 <q.bakha97@gmail.com>
2025-02-25 16:43:13 +0500

    Create diamond_butterfly.py

---
+++ lesson-7/homework/diamond_butterfly.py
@@ -0,0 +1,26 @@
+# size = 5    # diamond
+
+# for i in range(1, size + 1):
+#     print("  " * (size - i) + "*   " * i)
+
+# for i in range(size - 1, 0, -1):
+#     print("  " * (size - i) + "*   " * i)
+      
+
+# n = 5
+# for i in range(n):
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     for j in range(2 * (n - i - 1)):
+#         print(' ', end=' ')
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     print()
+# for i in range(n - 1, -1, -1):
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     for j in range(2 * (n - i - 1)):
+#         print(' ', end=' ')
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     print()                   // Butterfly // 
